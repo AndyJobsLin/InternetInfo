21f4b5693a9b89ac5dd42c1c1a770df284d48067
AndyJobs Lin <andyjobs.lin@gmail.com>
2018-05-22 09:21:21 +0800

    1. 於 Storyboard 上新增 Label 顯示 SSID 名稱 2. 新增取得 SSID 名稱方法

--- InternetInfo/ViewController.swift
+++ InternetInfo/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import SystemConfiguration.CaptiveNetwork
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Show: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Show.text = fetchSSIDInfo()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+//    func currentWifiSSID() -> String {
+//
+//        var ssid: String = "";
+//        if let interfaces = CNCopySupportedInterfaces() {
+//            let interfacesArray = interfaces() as! [String]
+//            if (interfacesArray.count > 0) {
+//
+//                let ifname = interfacesArray[0];
+//                let ifdata = CNCopyCurrentNetworkInfo(ifname);
+//                print("ifdata \(ifdata)")
+//                //println(ifdata)
+//                if(ifdata != nil)
+//                {
+//                    let interfaceData: Dictionary = ifdata.takeRetainedValue() as Dictionary
+//                    print("interfaceData \(interfaceData)")
+//                    ssid = interfaceData["SSID"] as! String
+//                }
+//            }
+//        }
+//
+//        return ssid;
+//    }
+    
+    func fetchSSIDInfo() ->  String? {
+        if let interfaces = CNCopySupportedInterfaces() {
+            for i in 0..<CFArrayGetCount(interfaces){
+                let interfaceName: UnsafeRawPointer = CFArrayGetValueAtIndex(interfaces, i)
+                let rec = unsafeBitCast(interfaceName, to: AnyObject.self)
+                let unsafeInterfaceData = CNCopyCurrentNetworkInfo("\(rec)" as CFString)
+                
+                if let unsafeInterfaceData = unsafeInterfaceData as? Dictionary<AnyHashable, Any> {
+                    return unsafeInterfaceData["SSID"] as? String
+                }
+            }
+        }
+        return nil
+    }
 
 }
 
